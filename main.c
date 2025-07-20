9f23af36fe8b77ac4f67057449d6fd364f6dff4d
Kayra Bora Keskin <kayrabora.keskin@gmail.com>
2025-07-20 14:02:57 +0300

    Commit 9 (With Bonus)

--- main.c
+++ main.c
@@ -1,17 +1,31 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
 #include <fcntl.h>
 
 int main(void)
 {
     char    *read;
+    char    *read2;
     int fd = open("Text.txt", O_RDWR, 0644);
+    int fd2 = open("Text2.txt", O_RDWR, 0644);
+    
+    read = get_next_line(fd);
+    read2 = get_next_line(fd2);
 
-    read = NULL;
-    while(read = get_next_line(fd))
+    while(read || read2)
     {
-        printf("%s", read);
-        free(read);
+        if (read)
+        {
+            printf("%s", read);
+            free(read);
+            read = get_next_line(fd);
+        }
+        if (read2)
+        {
+            printf("%s", read2);
+            free(read2);
+            read2 = get_next_line(fd2);
+        }
     }
 
     return (0);
